43b6641a6ff5154638df9d2ea01f3405b1ccae86
Xudong Liu <xudong.camsys@gmail.com>
2015-08-07 17:03:51 -0400

    remove deprecated migration script

--- db/migrate/20150414180638_create_reporting_specific_filter_groups.rb
+++ db/migrate/20150414180638_create_reporting_specific_filter_groups.rb
@@ -11,13 +11,11 @@ class CreateReportingSpecificFilterGroups < ActiveRecord::Migration
     add_index :reporting_specific_filter_groups, :reporting_report_id, name: 'index_of_report_on_specific_filter_group'
     add_index :reporting_specific_filter_groups, :reporting_filter_group_id, name: 'index_of_filter_group_on_specific_filter_group'
 
-    remove_reference :reporting_filter_groups, :report, index: true
     remove_column :reporting_filter_groups, :sort_order
   end
 
   def down
     add_column :reporting_filter_groups, :sort_order, :integer, null: false, default: 1
-    add_reference :reporting_filter_groups, :report, index: true
 
     drop_table :reporting_specific_filter_groups
   end
