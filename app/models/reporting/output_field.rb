732c92eca436131ef8843a946c89414860a01fcc
Xudong Liu <xudong.camsys@gmail.com>
2015-08-12 14:32:53 -0400

    correct OutputField validation

--- app/models/reporting/output_field.rb
+++ app/models/reporting/output_field.rb
@@ -4,6 +4,6 @@ module Reporting
 
     validates :name, presence: true
     validates :report, presence: true
-    validates :numeric_precision, :numericality => { :greater_than_or_equal_to => 0 }
+    validates :numeric_precision, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
   end
 end
